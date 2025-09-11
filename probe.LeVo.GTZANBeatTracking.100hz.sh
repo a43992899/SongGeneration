505f86be42fdefd0be263958ba5111e3ceb7ae80
a43992899 <a43992899@gmail.com>
2025-09-11 10:51:31 +0800

    save progress

--- probe.LeVo.GTZANBeatTracking.100hz.sh
+++ probe.LeVo.GTZANBeatTracking.100hz.sh
@@ -1,3 +1,3 @@
-rm -rf output/probe.GTZANBeatTracking.LeVo.vq_emb
-python cli.py fit -c probe.LeVo.GTZANBeatTracking.yaml
-python cli.py test -c probe.LeVo.GTZANBeatTracking.yaml
+rm -rf output/probe.GTZANBeatTracking.LeVo.vq_emb.100hz
+python cli.py fit -c probe.LeVo.GTZANBeatTracking.100hz.yaml
+python cli.py test -c probe.LeVo.GTZANBeatTracking.100hz.yaml
